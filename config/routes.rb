893076cf77d8ef2c247119349a76a9fceb768f40
willian <willian.l.lucio@gmail.com>
2018-02-05 09:24:07 -0200

    create model and controller clients

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'clients/index'
+
+  get 'clients/create'
+
+  get 'clients/new'
+
+  get 'clients/edit'
+
+  get 'clients/update'
+
+  get 'clients/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
